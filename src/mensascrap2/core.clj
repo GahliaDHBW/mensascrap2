742e77dea9447f57f934efea8ece1a2a0b3f9735
port19 <port19@port19.xyz>
2023-04-03 18:00:20 +0200

    feat: request wrapper function

--- src/mensascrap2/core.clj
+++ src/mensascrap2/core.clj
@@ -6,13 +6,23 @@
             [hickory.select :as s])
   (:gen-class))
 
-(def subject (->> "sample.html" slurp h/parse h/as-hickory))
+(def primary "https://www.imensa.de/karlsruhe")
+(defn request
+  [endpoint]
+  (let [response (client/get (str primary endpoint))]
+    (cond (= (:status response) 200) {:source primary :resp (-> (:body response) h/parse h/as-hickory)})))
 
+(def sample (request "/mensa-erzbergerstrasse/montag.html"))
+
+; TODO retarget to imensa
 ; bbyt select examples
 ;(s/select (s/attr "dir"))
 ;(s/select (s/class "length"))
 ;(s/select (s/descendant (s/child (s/and (s/attr "href") (s/attr "style")))))
 ; how do I select multiple times / how do I make selected stuff a document again?
+
+; HACK Old code lol
+(def subject (->> "sample.html" slurp h/parse h/as-hickory))
 (def relevantscope
   (->> subject
        (s/select (s/tag "table"))
