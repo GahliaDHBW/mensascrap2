e8cf0c8ec35357a985b003a2dc0fe1416a09648d
port19 <port19@port19.xyz>
2023-04-27 13:28:54 +0200

    fix: price per item

--- src/mensascrap2/core.clj
+++ src/mensascrap2/core.clj
@@ -22,10 +22,10 @@
                   (typecheck))
         price (->> patient
                    (s/select (s/class "aw-meal-price"))
-                   ((comp first :content first))
-                   (drop-last 2)
-                   (apply str)
+                   (str)
+                   (re-find #"\d\,\d\d")
                    (#(str/replace % "," ".")))]
+    #_(snipe "/mensa-moltke/montag.html")
     {:name name :type type :price price}))
 
 (defn snipe [endpoint]
@@ -38,35 +38,37 @@
        (s/select (s/class "aw-meal-category"))
        (map parse-metadata)))
 
-(defn -main []
-  (println (generate-string
-            {:head {:api-version "v2"
-                    :last-update (.toString (java.time.LocalDateTime/now))
-                    :source "www.imensa.de"}
-             :body {:Erzbergerstraße {:monday (snipe "/mensa-erzbergerstrasse/montag.html")
-                                      :tuesday (snipe "/mensa-erzbergerstrasse/dienstag.html")
-                                      :wednesday (snipe "/mensa-erzbergerstrasse/mittwoch.html")
-                                      :thursday (snipe "/mensa-erzbergerstrasse/donnerstag.html")
-                                      :friday (snipe "/mensa-erzbergerstrasse/freitag.html")}
-                    :Schloss-Gottesaue {:monay (snipe "/mensa-schloss-gottesaue/montag.html")
-                                        :tuesday (snipe "/mensa-schloss-gottesaue/dienstag.html")
-                                        :wednesday (snipe "/mensa-schloss-gottesaue/mittwoch.html")
-                                        :thursday (snipe "/mensa-schloss-gottesaue/donnerstag.html")
-                                        :friday (snipe "/mensa-schloss-gottesaue/freitag.html")}
-                    :Cafetaria-Moltkestraße {:monay (snipe "/cafeteria-moltkestrasse-30/montag.html")
-                                             :tuesday (snipe "/cafeteria-moltkestrasse-30/dienstag.html")
-                                             :wednesday (snipe "/cafeteria-moltkestrasse-30/mittwoch.html")
-                                             :thursday (snipe "/cafeteria-moltkestrasse-30/donnerstag.html")
-                                             :friday (snipe "/cafeteria-moltkestrasse-30/freitag.html")}
-                    :Mensa-Moltke {:monay (snipe "/mensa-moltke/montag.html")
-                                   :tuesday (snipe "/mensa-moltke/dienstag.html")
-                                   :wednesday (snipe "/mensa-moltke/mittwoch.html")
-                                   :thursday (snipe "/mensa-moltke/donnerstag.html")
-                                   :friday (snipe "/mensa-moltke/freitag.html")}
-                    :Mensa-Adenauerring {:monay (snipe "/mensa-am-adenauerring/montag.html")
-                                         :tuesday (snipe "/mensa-am-adenauerring/dienstag.html")
-                                         :wednesday (snipe "/mensa-am-adenauerring/mittwoch.html")
-                                         :thursday (snipe "/mensa-am-adenauerring/donnerstag.html")
-                                         :friday (snipe "/mensa-am-adenauerring/freitag.html")}}})))
+(defn buildedn []
+  {:head {:api-version "v2.1"
+          :last-update (.toString (java.time.LocalDateTime/now))
+          :source "www.imensa.de"}
+   :body {:Erzbergerstraße {:monday (snipe "/mensa-erzbergerstrasse/montag.html")
+                            :tuesday (snipe "/mensa-erzbergerstrasse/dienstag.html")
+                            :wednesday (snipe "/mensa-erzbergerstrasse/mittwoch.html")
+                            :thursday (snipe "/mensa-erzbergerstrasse/donnerstag.html")
+                            :friday (snipe "/mensa-erzbergerstrasse/freitag.html")}
+          :Schloss-Gottesaue {:monay (snipe "/mensa-schloss-gottesaue/montag.html")
+                              :tuesday (snipe "/mensa-schloss-gottesaue/dienstag.html")
+                              :wednesday (snipe "/mensa-schloss-gottesaue/mittwoch.html")
+                              :thursday (snipe "/mensa-schloss-gottesaue/donnerstag.html")
+                              :friday (snipe "/mensa-schloss-gottesaue/freitag.html")}
+          :Cafetaria-Moltkestraße {:monay (snipe "/cafeteria-moltkestrasse-30/montag.html")
+                                   :tuesday (snipe "/cafeteria-moltkestrasse-30/dienstag.html")
+                                   :wednesday (snipe "/cafeteria-moltkestrasse-30/mittwoch.html")
+                                   :thursday (snipe "/cafeteria-moltkestrasse-30/donnerstag.html")
+                                   :friday (snipe "/cafeteria-moltkestrasse-30/freitag.html")}
+          :Mensa-Moltke {:monay (snipe "/mensa-moltke/montag.html")
+                         :tuesday (snipe "/mensa-moltke/dienstag.html")
+                         :wednesday (snipe "/mensa-moltke/mittwoch.html")
+                         :thursday (snipe "/mensa-moltke/donnerstag.html")
+                         :friday (snipe "/mensa-moltke/freitag.html")}
+          :Mensa-Adenauerring {:monay (snipe "/mensa-am-adenauerring/montag.html")
+                               :tuesday (snipe "/mensa-am-adenauerring/dienstag.html")
+                               :wednesday (snipe "/mensa-am-adenauerring/mittwoch.html")
+                               :thursday (snipe "/mensa-am-adenauerring/donnerstag.html")
+                               :friday (snipe "/mensa-am-adenauerring/freitag.html")}}})
 
-(comment (-main))
+(defn -main []
+  (println (generate-string (buildedn))))
+
+#_(buildedn)
